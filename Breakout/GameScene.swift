801c24f1f6645776dec70eeca102e3a56f8eb623
Briana-Koo <RoyalBlueSnow@gmail.com>
2022-03-08 10:56:34 -0600

    Created brick object

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -11,19 +11,21 @@ import GameplayKit
 class GameScene: SKScene {
     var ball = SKShapeNode()
     var paddle = SKSpriteNode()
-
+    var brick = SKSpriteNode()
+    
     override func didMove(to view: SKView) {
         // this stuff happens once (when the app opens)
         createBackground()
-        makePaddle()
         resetGame()
     }
     
     func resetGame() {
-            // this stuff happens before each game starts
-            makeBall()
-        }
-     
+        // this stuff happens before each game starts
+        makeBall()
+        makePaddle()
+        makeBrick()
+    }
+    
     func createBackground() {
         let stars = SKTexture(imageNamed: "Stars")
         for i in 0...1 {
@@ -40,41 +42,50 @@ class GameScene: SKScene {
     }
     
     func makeBall() {
-           ball.removeFromParent()     // remove the ball (if it exists)
-           ball = SKShapeNode(circleOfRadius: 10)
-           ball.position = CGPoint(x: frame.midX, y: frame.midY)
-           ball.strokeColor = .black
-           ball.fillColor = .yellow
-           ball.name = "ball"
-           
-           // physics shape matches ball image
-           ball.physicsBody = SKPhysicsBody(circleOfRadius: 10)
-           // ignores all forces and impulses
-           ball.physicsBody?.isDynamic = false
-           // use precise collision detection
-           ball.physicsBody?.usesPreciseCollisionDetection = true
-           // no loss of energy from friction
-           ball.physicsBody?.friction = 0
-           // gravity is not a factor
-           ball.physicsBody?.affectedByGravity = false
-           // bounces fully off of other objects
-           ball.physicsBody?.restitution = 1
-           // does not slow down over time
-           ball.physicsBody?.linearDamping = 0
-           ball.physicsBody?.contactTestBitMask = (ball.physicsBody?.collisionBitMask)!
-           
-           addChild(ball)  // add ball object to the view
-       }
-
+        ball.removeFromParent()     // remove the ball (if it exists)
+        ball = SKShapeNode(circleOfRadius: 10)
+        ball.position = CGPoint(x: frame.midX, y: frame.midY)
+        ball.strokeColor = .black
+        ball.fillColor = .yellow
+        ball.name = "ball"
+        
+        // physics shape matches ball image
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: 10)
+        // ignores all forces and impulses
+        ball.physicsBody?.isDynamic = false
+        // use precise collision detection
+        ball.physicsBody?.usesPreciseCollisionDetection = true
+        // no loss of energy from friction
+        ball.physicsBody?.friction = 0
+        // gravity is not a factor
+        ball.physicsBody?.affectedByGravity = false
+        // bounces fully off of other objects
+        ball.physicsBody?.restitution = 1
+        // does not slow down over time
+        ball.physicsBody?.linearDamping = 0
+        ball.physicsBody?.contactTestBitMask = (ball.physicsBody?.collisionBitMask)!
+        
+        addChild(ball)  // add ball object to the view
+    }
+    
     func makePaddle() {
-            paddle.removeFromParent()   // remove the paddle, if it exists
-            paddle = SKSpriteNode(color: .white, size: CGSize(width: frame.width/4, height: 20))
-            paddle.position = CGPoint(x: frame.midX, y: frame.minY + 125)
-            paddle.name = "paddle"
-            paddle.physicsBody = SKPhysicsBody(rectangleOf: paddle.size)
-            paddle.physicsBody?.isDynamic = false
-            addChild(paddle)
-        }
-
+        paddle.removeFromParent()   // remove the paddle, if it exists
+        paddle = SKSpriteNode(color: .white, size: CGSize(width: frame.width/4, height: 20))
+        paddle.position = CGPoint(x: frame.midX, y: frame.minY + 125)
+        paddle.name = "paddle"
+        paddle.physicsBody = SKPhysicsBody(rectangleOf: paddle.size)
+        paddle.physicsBody?.isDynamic = false
+        addChild(paddle)
+    }
+    
+    func makeBrick() {
+        brick.removeFromParent()    // remove the brick, if it exists
+        brick = SKSpriteNode(color: .blue, size: CGSize(width: 50, height: 20))
+        brick.position = CGPoint(x: frame.midX, y: frame.maxY - 50)
+        brick.name = "brick"
+        brick.physicsBody = SKPhysicsBody(rectangleOf: brick.size)
+        brick.physicsBody?.isDynamic = false
+        addChild(brick)
+    }
 }
 
